9b4de73d0e707aca73f58c9fe2a5556a9937995c
1 <a1@wangxiangwei.local>
2018-06-11 09:13:15 +0800

    Initial Commit

---
+++ learning/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  learning
+//
+//  Created by 祥伟 on 2018/6/11.
+//  Copyright © 2018年 wanda. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ learning/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  learning
+//
+//  Created by 祥伟 on 2018/6/11.
+//  Copyright © 2018年 wanda. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
